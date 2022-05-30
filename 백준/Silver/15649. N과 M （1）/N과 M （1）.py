35f2955b78b23cfc52d4eb4340e85ba8728240aa
Bosub Kim <39071638+boompatron@users.noreply.github.com>
2022-05-30 14:43:42 +0900

    [Silver III] Title: N과 M (1), Time: 220 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -0,0 +1,20 @@
+import sys
+
+
+def recursive(n, m, s):
+    if len(s) == m:
+        print(' '.join(map(str, s)))
+    else:
+        for i in range(1, n + 1):
+            if i in s:
+                continue
+            recursive(n, m, s + [i])
+
+
+def solution():
+    n, m = map(int, sys.stdin.readline().rstrip().split())
+    recursive(n, m, [])
+
+
+if __name__ == "__main__":
+    solution()
