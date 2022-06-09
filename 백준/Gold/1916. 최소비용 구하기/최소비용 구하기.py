24a81748d7c90d680ee3077962d55dab08d4dabe
Bosub Kim <39071638+boompatron@users.noreply.github.com>
2022-06-09 13:31:50 +0900

    [Gold V] Title: 최소비용 구하기, Time: 368 ms, Memory: 58704 KB -BaekjoonHub

---
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -0,0 +1,36 @@
+import sys
+import heapq
+
+
+def dijkstra(start, ans_list, adj_list):
+    ans_list[start] = 0
+    hq = []
+    heapq.heappush(hq, [0, start])
+    while len(hq):
+        dis, cur = heapq.heappop(hq)
+        if ans_list[cur] < dis:
+            # 현재 ans[cur]에 들어있는 값이 현재보다 더 작으므로 갱신할 필요가 없음 그래서 넘어감
+            continue
+        # 하지만 이제 더 작아서 갱신이 가능하다면
+        # 그 점이랑 연결된 점들과의 거리르 비교해서 ans를 갱싱해야 함
+        for a in adj_list[cur]: # 현재 점들과 연결된 점들을 search
+            next_num, next_dis = a[0], a[1] + dis # 다음 점 위치와 다음 점 까지의 거리 : 지금가지의 거리(dis) + adj에 저장된 거리
+            if next_dis < ans_list[next_num]: # 만약 ans에 저장된 값보다 더 작으면 수색할 가치가 있다고 판단, hq에 넣음
+                ans_list[next_num] = next_dis # 값을 갱신함
+                heapq.heappush(hq, [next_dis, next_num])
+
+
+def solution():
+    v = int(sys.stdin.readline().rstrip())
+    e = int(sys.stdin.readline().rstrip())
+    adj, ans = [[] for _ in range(v + 1)], [123456789 for i in range(v + 1)]
+    for _ in range(e):
+        a, b, c = map(int, sys.stdin.readline().rstrip().split())
+        adj[a].append([b, c])
+    start, end = map(int, sys.stdin.readline().rstrip().split())
+    dijkstra(start, ans, adj)
+    print(ans[end])
+
+
+if __name__ == "__main__":
+    solution()
