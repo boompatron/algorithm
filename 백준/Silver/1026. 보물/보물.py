40ccdae7dfd8a96b16e605cc6a5f6cb5b7d8c6c5
Bosub Kim <39071638+boompatron@users.noreply.github.com>
2022-07-25 21:40:22 +0900

    [Silver IV] Title: 보물, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1026. 보물/보물.py
+++ 백준/Silver/1026. 보물/보물.py
@@ -1,20 +1,11 @@
 import sys
-import heapq
 
 
 def solution():
     n = int(sys.stdin.readline().rstrip())
-    a_hq, b_hq = [], []
-    A = list(map(int, sys.stdin.readline().rstrip().split()))
-    B = list(map(int, sys.stdin.readline().rstrip().split()))
-    for a, b in zip(A, B):
-        heapq.heappush(a_hq, -a)
-        heapq.heappush(b_hq, b)
-    ans = 0
-    while n:
-        ans -= heapq.heappop(a_hq) * heapq.heappop(b_hq)
-        n -= 1
-    print(ans)
+    A = sorted(map(int, sys.stdin.readline().rstrip().split()))
+    B = sorted(map(int, sys.stdin.readline().rstrip().split()), key=lambda x: -x)
+    print(sum([a*b for a, b in zip(A, B)]))
 
 
 if __name__ == "__main__":
