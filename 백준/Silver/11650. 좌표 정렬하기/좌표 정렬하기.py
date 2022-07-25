c3f4263446ffa3ca6b7d82e875fe85470defbbbb
Bosub Kim <39071638+boompatron@users.noreply.github.com>
2022-07-25 22:56:26 +0900

    [Silver V] Title: 좌표 정렬하기, Time: 404 ms, Memory: 58544 KB -BaekjoonHub

--- 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
+++ 백준/Silver/11650. 좌표 정렬하기/좌표 정렬하기.py
@@ -1,9 +1,15 @@
-n = int(input())
-coord = [[0, 0] for i in range(n)]
-for i in range(n):
-    xpos, ypos = map(int, input().split())
-    coord[i][0] = xpos
-    coord[i][1] = ypos
-l = sorted(coord, key=lambda x: (x[0], x[1]))
-for i in range(len(l)):
-    print(l[i][0], l[i][1], sep=" ")
+import sys
+
+
+def solution():
+    n = int(sys.stdin.readline().rstrip())
+    g = [[] for _ in range(n)]
+    for i in range(n):
+        g[i] = list(map(int, sys.stdin.readline().rstrip().split()))
+    for i in sorted(g, key=lambda x: (x[0], x[1])): # 우선 x 좌표를 오름차순으로 정렬하고
+																										# x 좌표가 같으면 y 좌표를 오름차순으로 정렬한다
+        print(' '.join(map(str, i)))
+
+
+if __name__ == "__main__":
+    solution()
