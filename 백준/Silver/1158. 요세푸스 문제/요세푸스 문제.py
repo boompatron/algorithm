97be9c2e13a663090692f379e40905958e284f3d
Bosub Kim <39071638+boompatron@users.noreply.github.com>
2022-08-08 22:16:25 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 156 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.py
@@ -1,12 +1,18 @@
-n, k = map(int, input().split())
+import sys
 
-nums = list(range(1, n + 1))
-idx = k - 1
-ans = []
-while True:
-    ans.append(nums[idx])
-    nums = nums[:idx] + nums[idx + 1:]
-    if not nums:
-        break
-    idx = (idx + k - 1) % len(nums)
-print('<' + ', '.join(map(str, ans)) + '>')
+
+def solution():
+    n, k = map(int, sys.stdin.readline().rstrip().split())
+    josephus, pos, ans = [_ for _ in range(1, n + 1)], k - 1, []
+    for i in range(n):
+        ans.append(josephus[pos % n])
+        josephus = josephus[:pos] + josephus[pos + 1:]
+        if not josephus:
+            break
+        pos += k - 1
+        pos = pos % len(josephus)
+    print('<' + ', '.join(map(str, ans)) + '>')
+
+
+if __name__ == "__main__":
+    solution()
