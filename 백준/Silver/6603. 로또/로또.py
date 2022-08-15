9edb1d3fbc3c8cf5d4a8b27608c5d815f6aa0f1d
Bosub Kim <39071638+boompatron@users.noreply.github.com>
2022-08-15 22:51:13 +0900

    [Silver II] Title: 로또, Time: 80 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.py
+++ 백준/Silver/6603. 로또/로또.py
@@ -1,21 +1,14 @@
 import sys
-
-
-def recursive(g, s, n):
-    if len(s) == 6:
-        print(' '.join(map(str, s)))
-    else:
-        for i in range(n):
-            if g[i] in s or len(s) and max(s) > g[i]:
-                continue
-            recursive(g, s + [g[i]], n)
+from itertools import combinations
 
 
 def solution():
     while True:
         g = list(map(int, sys.stdin.readline().rstrip().split()))
-        # print(g, g[1:])
-        exit(0) if len(g) == 1 else recursive(g[1:], [], len(g) - 1)
+        if len(g) == 1:
+            exit(0)
+        for c in combinations(g[1:], 6):
+            print(*c)
         print()
 
 
