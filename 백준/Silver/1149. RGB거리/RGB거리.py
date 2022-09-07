f900c82e52572d784b36debe29dd799ae3d5965b
Bosub Kim <39071638+boompatron@users.noreply.github.com>
2022-09-07 10:00:43 +0900

    [Silver I] Title: RGB거리, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -1,22 +1,24 @@
 import sys
+dxs = [-2, -1, 1, 2]
 
-n = int(sys.stdin.readline())
-price = [[0] for i in range(3)]
-for i in range(n):
-    r_tmp, g_tmp, b_tmp = map(int, sys.stdin.readline().split())
-    price[0].append(r_tmp)
-    price[1].append(g_tmp)
-    price[2].append(b_tmp)
-for i in range(1, n + 1):
-    for j in range(3):
-        tmp_minimum = 1234567
-        for k in range(3):
-            if j != k:
-                if price[j][i] + price[k][i - 1] < tmp_minimum:
-                    tmp_minimum = price[j][i] + price[k][i - 1]
-        price[j][i] = tmp_minimum
-ans = 1234567
-for i in range(3):
-    if price[i][n] < ans:
-        ans = price[i][n]
-print(ans)
+
+def in_range(x):
+    return 0 <= x < 3
+
+
+def solution():
+    n = int(sys.stdin.readline().rstrip())
+    g = [list(map(int, sys.stdin.readline().rstrip().split())) for _ in range(n)]
+    for i in range(1, n):
+        for x in range(3):
+            tmp = sys.maxsize
+            for dx in dxs:
+                nx = x + dx
+                if in_range(nx) and g[i - 1][nx] < tmp:
+                    tmp = g[i - 1][nx]
+            g[i][x] += tmp
+    print(min(g[n - 1]))
+
+
+if __name__ == "__main__":
+    solution()
