ea80afb1c2a70ef32478c24d16b0874f127686b5
Bosub Kim <39071638+boompatron@users.noreply.github.com>
2022-09-20 14:42:18 +0900

    [Gold IV] Title: 최단경로, Time: 764 ms, Memory: 159024 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.py
+++ 백준/Gold/1753. 최단경로/최단경로.py
@@ -1,38 +1,34 @@
 import sys
 import heapq
+INF = sys.maxsize
 
 
-def dijkstra(start, ans_list, adj_list):
-    ans_list[start] = 0
+def dijkstra(start, adj, ans):
+    ans[start] = 0
     hq = []
     heapq.heappush(hq, [0, start])
-    while len(hq):
-        dis, cur = heapq.heappop(hq)
-        if ans_list[cur] < dis:
-            # 현재 ans[cur]에 들어있는 값이 현재보다 더 작으므로 갱신할 필요가 없음 그래서 넘어감
+    while hq:
+        dis, idx = heapq.heappop(hq)
+        if ans[idx] < dis:
             continue
-        # 하지만 이제 더 작아서 갱신이 가능하다면
-        # 그 점이랑 연결된 점들과의 거리르 비교해서 ans를 갱싱해야 함
-        for a in adj_list[cur]: # 현재 점들과 연결된 점들을 search
-            next_num, next_dis = a[0], a[1] + dis # 다음 점 위치와 다음 점 까지의 거리 : 지금가지의 거리(dis) + adj에 저장된 거리
-            if next_dis < ans_list[next_num]: # 만약 ans에 저장된 값보다 더 작으면 수색할 가치가 있다고 판단, hq에 넣음
-                ans_list[next_num] = next_dis # 값을 갱신함
-                heapq.heappush(hq, [next_dis, next_num])
+        for a in adj[idx]:
+            next_idx, next_dis = a[0], a[1] + dis
+            if ans[next_idx] > next_dis:
+                ans[next_idx] = next_dis
+                heapq.heappush(hq, [next_dis, next_idx])
+    return ans[1:]
 
 
 def solution():
     v, e = map(int, sys.stdin.readline().rstrip().split())
-    adj, ans = [[] for _ in range(v + 1)], [123456789 for i in range(v + 1)]
-    start_vertex = int(sys.stdin.readline().rstrip())
-    for _ in range(e):
+    k = int(sys.stdin.readline().rstrip())
+    adj, ans = [[] for _ in range(v + 1)], [INF for _ in range(v + 1)]
+    while e:
         a, b, c = map(int, sys.stdin.readline().rstrip().split())
         adj[a].append([b, c])
-    dijkstra(start_vertex, ans, adj)
-    for i in range(1, v + 1):
-        if ans[i] == 123456789:
-            print("INF")
-        else:
-            print(ans[i])
+        e -= 1
+    for d in dijkstra(k, adj, ans):
+        print(d if d != INF else 'INF')
 
 
 if __name__ == "__main__":
