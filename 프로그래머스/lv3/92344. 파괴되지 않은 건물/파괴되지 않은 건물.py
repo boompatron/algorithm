baf01acce7612966a7295dac79ba59f8e2912767
Bosub Kim <39071638+boompatron@users.noreply.github.com>
2022-09-29 10:53:14 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 624.82 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/lv3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.py
+++ 프로그래머스/lv3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.py
@@ -1,21 +1,20 @@
 def solution(board, skill):
-    n, m = len(board), len(board[0])
-    prefix_sum_board = [[0 for _ in range(m + 1)] for __ in range(n + 1)]
-    for is_attack, y1, x1, y2, x2, degree in skill:
-        val = -degree if is_attack == 1 else degree
-        prefix_sum_board[y1][x1] += val
-        prefix_sum_board[y2 + 1][x2 + 1] += val
-        prefix_sum_board[y2 + 1][x1] += -val
-        prefix_sum_board[y1][x2 + 1] += -val
-    for i in range(n):
-        for j in range(1, m):
-            prefix_sum_board[i][j] += prefix_sum_board[i][j - 1]
-    for j in range(m):
-        for i in range(1, n):
-            prefix_sum_board[i][j] += prefix_sum_board[i - 1][j]
-    cnt = 0
+    n, m, cnt = len(board), len(board[0]), 0
+    prefix_sum = [[0 for _ in range(m + 1)] for __ in range(n + 1)]
+    for attak_type, y1, x1, y2, x2, degree in skill:
+        val = -degree if attak_type == 1 else degree
+        prefix_sum[y1][x1] += val
+        prefix_sum[y2 + 1][x2 + 1] += val
+        prefix_sum[y1][x2 + 1] -= val
+        prefix_sum[y2 + 1][x1] -= val
+    for i in range(n + 1):
+        for j in range(m):
+            prefix_sum[i][j + 1] += prefix_sum[i][j]
+    for j in range(m + 1):
+        for i in range(n):
+            prefix_sum[i + 1][j] += prefix_sum[i][j]
     for i in range(n):
         for j in range(m):
-            if board[i][j] + prefix_sum_board[i][j] > 0:
+            if board[i][j] > -prefix_sum[i][j]:
                 cnt += 1
     return cnt
