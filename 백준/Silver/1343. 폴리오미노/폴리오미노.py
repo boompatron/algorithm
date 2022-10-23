3bf3c745f41e4af358263298b5b84787f2588ceb
Bosub Kim <grapeflavoredapple@gmail.com>
2022-10-23 14:58:43 +0900

    [Silver V] Title: 폴리오미노, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1343. 폴리오미노/폴리오미노.py
+++ 백준/Silver/1343. 폴리오미노/폴리오미노.py
@@ -3,8 +3,7 @@ import sys
 
 def main():
     g = str(sys.stdin.readline().rstrip())
-    g = g.replace('XXXX', 'AAAA')
-    g = g.replace('XX', 'BB')
+    g = g.replace('XXXX', 'AAAA').replace('XX', 'BB')
     print(g if 'X' not in g else -1)
 
 
