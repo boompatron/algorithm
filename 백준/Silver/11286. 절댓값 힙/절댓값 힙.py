e1e8d7b37753d3b33317ca857b834e4442d4715c
Bosub Kim <grapeflavoredapple@gmail.com>
2022-12-14 10:17:47 +0900

    [Silver I] Title: 절댓값 힙, Time: 176 ms, Memory: 39780 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
@@ -1,24 +1,16 @@
 import sys
 import heapq
 
-
-def solution():
-    n = int(sys.stdin.readline().rstrip())
-    hq = []
-    while n:
-        a = int(sys.stdin.readline().rstrip())
-        if a:
-            heapq.heappush(hq, [abs(a), False if a < 0 else True])
+n = int(sys.stdin.readline().rstrip())
+hq = []
+while n:
+    a = int(sys.stdin.readline().rstrip())
+    if a:
+        heapq.heappush(hq, [abs(a), a > 0])
+    else:
+        if not hq:
+            print(0)
         else:
-            if hq:
-                num, is_positive = heapq.heappop(hq)
-                print(num if is_positive else num * -1)
-            else:
-                print(0)
-        n -= 1
-
-
-if __name__ == "__main__":
-    solution()
-
-
+            val, isPositive = heapq.heappop(hq)
+            print(val if isPositive else -val)
+    n -= 1
