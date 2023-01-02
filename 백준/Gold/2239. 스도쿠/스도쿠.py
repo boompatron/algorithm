cd962eaf2be5af6908783991637f924b32e9a90f
Bosub Kim <grapeflavoredapple@gmail.com>
2023-01-02 14:47:07 +0900

    [Gold IV] Title: 스도쿠, Time: 6544 ms, Memory: 139664 KB -BaekjoonHub

---
+++ 백준/Gold/2239. 스도쿠/스도쿠.py
@@ -0,0 +1,44 @@
+import sys
+
+
+def promising_nums(g, x, y, check):
+    for i in range(9):
+        if g[i][x] in check:
+            check.remove(g[i][x])
+        if g[y][i] in check:
+            check.remove(g[y][i])
+    for r in range(y // 3 * 3, y // 3 * 3 + 3):
+        for c in range(x // 3 * 3, x // 3 * 3 + 3):
+            if g[r][c] in check:
+                check.remove(g[r][c])
+
+
+def sudoku(g, zero, depth):
+    if len(zero) == depth:
+        for i in g:
+            for j in i:
+                print(j, end='')
+            print()
+        exit(0)
+    check = {'1', '2', '3', '4', '5', '6', '7', '8', '9'}
+    # check = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+    cur = zero[depth]
+    promising_nums(g, *cur, check)
+    for c in check:
+        g[cur[1]][cur[0]] = c
+        sudoku(g, zero, depth + 1)
+        g[cur[1]][cur[0]] = 0
+
+
+def solution():
+    g = [list(map(str, sys.stdin.readline().rstrip())) for _ in range(9)]
+    zero = []
+    for y, e1 in enumerate(g):
+        for x, e2 in enumerate(e1):
+            if e2 == '0':
+                zero.append([x, y])
+    sudoku(g, zero, 0)
+
+
+if __name__ == "__main__":
+    solution()
