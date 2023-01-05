ea4c449081bfa7a436a2ea6d442fbe244bf14118
Bosub Kim <grapeflavoredapple@gmail.com>
2023-01-05 15:45:53 +0900

    [Gold III] Title: 줄 세우기, Time: 168 ms, Memory: 39972 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.py
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -1,28 +1,29 @@
 import sys
-from collections import deque
 
 
 def solution():
     n, m = map(int, sys.stdin.readline().rstrip().split())
-    adj, degree = [[] for _ in range(n + 1)], [0 for _ in range(n + 1)]
+    in_degree, g = [0 for _ in range(n + 1)], [[] for _ in range(n + 1)]
     while m:
         a, b = map(int, sys.stdin.readline().rstrip().split())
-        adj[a].append(b)
-        degree[b] += 1
+        in_degree[b] += 1
+        g[a].append(b)
         m -= 1
-    dq, ans = deque(), deque()
-    for i, e in enumerate(degree[1:], start=1):
-        if not e:
-            dq.appendleft(i)
-    while dq:
-        cur = dq.pop()
+    q, ans = [], []
+
+    for i in range(1, n + 1):
+        if in_degree[i] == 0:
+            q.append(i)
+
+    while q:
+        cur = q.pop()
         ans.append(cur)
-        for a in adj[cur]:
-            degree[a] -= 1
-            if not degree[a]:
-                dq.appendleft(a)
-    print(' '.join(map(str, ans)))
+        for i in g[cur]:
+            in_degree[i] -= 1
+            if in_degree[i] == 0:
+                q.append(i)
+    print(" ".join(map(str, ans)))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     solution()
