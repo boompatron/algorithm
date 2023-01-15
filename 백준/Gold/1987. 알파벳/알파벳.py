adac53b401b73989764d5b322421f5f384f6c485
Bosub Kim <grapeflavoredapple@gmail.com>
2023-01-15 18:15:42 +0900

    [Gold IV] Title: 알파벳, Time: 6596 ms, Memory: 175364 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -3,20 +3,20 @@ import sys
 dxs, dys = (1, -1, 0, 0), (0, 0, 1, -1)
 n, m = map(int, sys.stdin.readline().rstrip().split())
 g = [list(map(str, sys.stdin.readline().rstrip())) for _ in range(n)]
-visited = [False for _ in range(26)]
-visited[ord(g[0][0]) - 65] = True
+visited = 0
+visited |= (1 << (ord(g[0][0]) - 65))
 ans = 0
 
 
 def dfs(x, y, cnt):
-    global ans
+    global ans, visited
     ans = max(cnt, ans)
     for i in range(4):
         nx, ny = x + dxs[i], y + dys[i]
-        if 0 <= nx < m and 0 <= ny < n and not visited[ord(g[ny][nx]) - 65]:
-            visited[ord(g[ny][nx]) - 65] = True
+        if 0 <= nx < m and 0 <= ny < n and not (visited & (1 << (ord(g[ny][nx]) - 65))):
+            visited |= (1 << (ord(g[ny][nx]) - 65))
             dfs(nx, ny, cnt + 1)
-            visited[ord(g[ny][nx]) - 65] = False
+            visited &= ~(1 << (ord(g[ny][nx]) - 65))
 
 
 dfs(0, 0, 1)
