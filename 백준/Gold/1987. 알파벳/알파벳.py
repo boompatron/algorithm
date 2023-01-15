f777857521545d5a6d585b4fef3c0539a7420b24
Bosub Kim <grapeflavoredapple@gmail.com>
2023-01-15 17:17:09 +0900

    [Gold IV] Title: 알파벳, Time: 6760 ms, Memory: 178640 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,26 +1,23 @@
-r, c = map(int, input().split())
-graph = [list(input()) for _ in range(r)]
-visited = set()
-dx, dy = (-1, 1, 0, 0), (0, 0, -1, 1)
+import sys
+
+dxs, dys = (1, -1, 0, 0), (0, 0, 1, -1)
+n, m = map(int, sys.stdin.readline().rstrip().split())
+g = [list(map(str, sys.stdin.readline().rstrip())) for _ in range(n)]
+visited = [False for _ in range(26)]
+visited[ord(g[0][0]) - 65] = True
 ans = 0
 
 
 def dfs(x, y, cnt):
     global ans
-
-    ans = max(ans, cnt)
-    visited.add(graph[x][y])
-
+    ans = max(cnt, ans)
     for i in range(4):
-        nx, ny = x + dx[i], y + dy[i]
-
-        if 0 <= nx < r and 0 <= ny < c:
-            if graph[nx][ny] not in visited:
-                dfs(nx, ny, cnt + 1)
-
-    visited.remove(graph[x][y])
+        nx, ny = x + dxs[i], y + dys[i]
+        if 0 <= nx < m and 0 <= ny < n and not visited[ord(g[ny][nx]) - 65]:
+            visited[ord(g[ny][nx]) - 65] = True
+            dfs(nx, ny, cnt + 1)
+            visited[ord(g[ny][nx]) - 65] = False
 
 
 dfs(0, 0, 1)
-
 print(ans)
