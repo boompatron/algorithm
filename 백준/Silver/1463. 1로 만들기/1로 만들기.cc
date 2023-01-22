a09562957a4f3ab1e88395d7a699aa42e5ac3ebd
Bosub Kim <grapeflavoredapple@gmail.com>
2023-01-22 19:16:26 +0900

    [Silver III] Title: 1로 만들기, Time: 8 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,21 +1,16 @@
 #include <iostream>
 using namespace std;
+int dp[1000001];
 int main() {
-	int num, dp[1000001];
-	cin >> num;
-	dp[1] = 0;
-	dp[2] = 1;
-	dp[3] = 1;
-	for (int i = 4; i <= num; i++) {
-		if (i % 3 == 0 && i % 2 == 0)
-			dp[i] = min(min(dp[i - 1], dp[i / 3]), dp[i / 2]) + 1;
-		else if (i % 3 == 0)
-			dp[i] = min(dp[i - 1], dp[i / 3]) + 1;
-        else if (i % 2 == 0)
-			dp[i] = min(dp[i - 1], dp[i / 2]) + 1;
-		else
-			dp[i] = dp[i - 1] + 1;
+	ios_base::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+	int N;
+	cin >> N;
+	for (int i = N; i > 0; i--) {
+		if (i % 3 == 0)	dp[i / 3] = dp[i] + 1;
+		if (i % 2 == 0)	dp[i / 2] = dp[i / 2] ? min(dp[i / 2], dp[i] + 1) : dp[i] + 1;
+		dp[i - 1] = dp[i - 1] ? min(dp[i - 1], dp[i] + 1) : dp[i] + 1;
 	}
-	cout << dp[num];
+	cout << dp[1] << endl;
 	return 0;
 }
