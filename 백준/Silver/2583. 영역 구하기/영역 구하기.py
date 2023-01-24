355836eadcaf06ac960075d773e152f27df45a76
Bosub Kim <grapeflavoredapple@gmail.com>
2023-01-24 17:55:12 +0900

    [Silver I] Title: 영역 구하기, Time: 72 ms, Memory: 34176 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.py
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.py
@@ -1,40 +1,51 @@
 import sys
 from collections import deque
+visited, g = [[]], [[]]
+n, m = 0, 0
 dxs, dys = [1, -1, 0, 0], [0, 0, 1, -1]
-n, m, k = map(int, sys.stdin.readline().rstrip().split())
-g, visited = [[True for _ in range(m)] for __ in range(n)], [[False for _ in range(m)] for __ in range(n)]
-ans = []
-for _ in range(k):
-    x1, y1, x2, y2 = map(int, sys.stdin.readline().rstrip().split())
-    for i in range(y1, y2):
-        for j in range(x1, x2):
-            g[i][j] = False
 
 
-def in_range(a, b):
-    return 0 <= a < m and 0 <= b < n
+def in_range(x, y):
+    global n, m
+    return 0 <= x < m and 0 <= y < n
 
 
-def bfs(a, b):
-    cnt = 1
-    visited[b][a] = True
+def bfs(xx, yy):
+    global g, visited, n, m
     dq = deque()
-    dq.appendleft([a, b])
+    dq.appendleft([xx, yy])
+    cnt = 1
+    visited[yy][xx] = True
     while dq:
         x, y = dq.pop()
         for dx, dy in zip(dxs, dys):
             nx, ny = x + dx, y + dy
-            if in_range(nx, ny) and not visited[ny][nx] and g[ny][nx]:
+            if in_range(nx, ny) and not g[ny][nx] and not visited[ny][nx]:
                 dq.appendleft([nx, ny])
                 visited[ny][nx] = True
                 cnt += 1
     return cnt
 
 
-for i in range(n):
-    for j in range(m):
-        if g[i][j] and not visited[i][j]:
-            ans.append(bfs(j, i))
-print(len(ans))
-for i in sorted(ans):
-    print(i)
+def solution():
+    global n, m, g, visited
+    n, m, k = map(int, sys.stdin.readline().rstrip().split())
+    g, visited = [[False for _ in range(m)] for __ in range(n)], [[False for _ in range(m)] for __ in range(n)]
+    while k:
+        x1, y1, x2, y2 = map(int, sys.stdin.readline().rstrip().split())
+        for y in range(y1, y2):
+            for x in range(x1, x2):
+                g[y][x] = True
+        k -= 1
+    ans = []
+    for y, e1 in enumerate(g):
+        for x, e2 in enumerate(e1):
+            if not e2 and not visited[y][x]:
+                ans.append(bfs(x, y))
+    print(len(ans))
+    for i in sorted(ans):
+        print(i, end=" ")
+
+
+if __name__ == "__main__":
+    solution()
