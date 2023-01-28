2bca2d11ed6d14696650d43d6a0cb4e29f976cd4
Bosub Kim <grapeflavoredapple@gmail.com>
2023-01-28 20:38:20 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.py
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.py
@@ -1,6 +1,5 @@
-a, b, v = map(int, input().split())
-(v - a) // (a - b)
-if (v - a) % (a - b) == 0:
-    print((v - a) // (a - b) + 1)
-else:   #(v - a) % (a - b) != 0
-    print((v - a) // (a - b) + 2)
+import sys
+
+
+a, b, v = map(int, sys.stdin.readline().rstrip().split())
+print(((v - a) // (a - b) if (v - a) % (a - b) == 0 else (v - a) // (a - b) + 1) + 1)
