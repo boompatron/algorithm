190ce6bbabb16f48b963024c317639d0105dad38
Bosub Kim <grapeflavoredapple@gmail.com>
2023-04-12 00:44:05 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -1,26 +1,21 @@
-#include <cstdio>
-
-int max(int a, int b) { return a > b ? a : b; }
+#include <iostream>
+#include <algorithm>
+using namespace std;
 int main() {
-
-    int N, i, j, ans = 0;
-
-    int dp[1000];
-    int arr[1000];
-
-    scanf("%d", &N);
-
-    for (i = 0; i < N; i++) scanf("%d", &arr[i]);
-
-    for (i = 0; i < N; i++) {
-        dp[i] = 1;
-        for (j = 0; j < i; j++) {
-            if (arr[i] > arr[j] && dp[j] + 1 > dp[i]) dp[i] = dp[j] + 1;
-        }
-    }
-
-    for (i = 0; i < N; i++) ans = max(ans, dp[i]);
-    printf("%d\n", ans);
-
-    return 0;
+	int size, arr[1000], dp[1000] = {1}, answer = 0;
+	cin >> size;
+	for (int i = 0; i < size; i++) {
+		cin >> arr[i];
+		dp[i] = 1;
+	}
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < i; j++) {
+			if (arr[i] > arr[j] && dp[i] < dp[j] + 1) {
+				dp[i] = dp[j] + 1;
+			}
+		}
+		answer = max(answer, dp[i]);
+	}
+	cout << answer;
+	return 0;
 }
