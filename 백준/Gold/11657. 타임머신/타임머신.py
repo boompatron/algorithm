8bd339f26d697d2823efb96948a604b70868e8f2
Bosub Kim <grapeflavoredapple@gmail.com>
2023-04-13 21:23:29 +0900

    [Gold IV] Title: 타임머신, Time: 524 ms, Memory: 32276 KB -BaekjoonHub

--- 백준/Gold/11657. 타임머신/타임머신.py
+++ 백준/Gold/11657. 타임머신/타임머신.py
@@ -17,18 +17,19 @@ def bellman_ford(adj, ans, start, v, e):
 def solution():
     v, e = map(int, sys.stdin.readline().rstrip().split())
     adj, ans = [], [INF for _ in range(v + 1)]
-    for _ in range(e):
+    for i in range(e):
         a, b, c = map(int, sys.stdin.readline().rstrip().split())
         adj.append([a, b, c])
     if bellman_ford(adj, ans, 1, v, e):
         print(-1)
     else:
-        for i in range(2, v + 1):
-            if ans[i] == INF:
+        for a in ans[2:]:
+            if a == INF:
                 print(-1)
             else:
-                print(ans[i])
+                print(a)
 
 
 if __name__ == "__main__":
     solution()
+    
