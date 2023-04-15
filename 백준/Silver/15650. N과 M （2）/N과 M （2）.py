6566bc54c1727ef17d72933a4df440fceac394e6
Bosub Kim <grapeflavoredapple@gmail.com>
2023-04-15 19:48:34 +0900

    [Silver III] Title: N과 M (2), Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,15 +1,7 @@
 import sys
+from itertools import combinations
+
 n, m = map(int, sys.stdin.readline().rstrip().split())
-
-
-def f(s):
-    if len(s) == m:
-        print(' '.join(map(str, s)))
-        return
-    for i in range(1, n + 1):
-        if i in s or (len(s) and i < s[-1]):
-            continue
-        f(s + [i])
-
-
-f([])
+for comb in combinations([i for i in range(1, n + 1)], m):
+    print(*comb)
+    
