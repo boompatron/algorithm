a6ae121ac22a9931f6a65d522bc1ffa765fa0ff8
Bosub Kim <grapeflavoredapple@gmail.com>
2023-04-15 23:54:13 +0900

    [Silver III] Title: N과 M (8), Time: 56 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/15657. N과 M （8）/N과 M （8）.py
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.py
@@ -1,17 +1,7 @@
 import sys
+from itertools import combinations_with_replacement
+
 n, m = map(int, sys.stdin.readline().rstrip().split())
-l = list(map(int, sys.stdin.readline().rstrip().split()))
-l.sort()
-
-
-def f(s):
-    if len(s) == m:
-        print(' '.join(map(str, s)))
-        return
-    for i in l:
-        if len(s) and i < s[-1]:
-            continue
-        f(s + [i])
-
-
-f([])
+g = list(map(int, sys.stdin.readline().rstrip().split()))
+for comb_with_rep in combinations_with_replacement(sorted(g), m):
+    print(*comb_with_rep)
