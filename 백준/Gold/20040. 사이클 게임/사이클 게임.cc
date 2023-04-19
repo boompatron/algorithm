345800dc9b4325aab9d9c8b8d338549afcc54711
Bosub Kim <grapeflavoredapple@gmail.com>
2023-04-19 16:00:16 +0900

    [Gold IV] Title: 사이클 게임, Time: 100 ms, Memory: 3972 KB -BaekjoonHub

--- 백준/Gold/20040. 사이클 게임/사이클 게임.cc
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.cc
@@ -1,36 +1,40 @@
-#include <iostream>
-#include <set>
+#include <bits/stdc++.h>
 using namespace std;
-int n, m;
+int v, e;
 int parent[500001];
-int get_parent(int parent[], int x) {
-	if (parent[x] == x) return x;
-	return parent[x] = get_parent(parent, parent[x]);
+int get_parent(int p[], int x){
+    if (p[x] != x) {
+        p[x] = get_parent(p, p[x]);
+    }
+    return p[x];
 }
-void union_parent(int parent[], int a, int b) {
-	a = get_parent(parent, a);
-	b = get_parent(parent, b);
-	if (a < b)		parent[b] = a;
-	else if (a > b) parent[a] = b;
-	else			return;
+void union_parent(int p[], int a, int b){
+    a = get_parent(p, a);
+    b = get_parent(p, b);
+    if (a > b)
+        p[a] = b;
+    else if (a < b)
+        p[b] = a;
+    else
+        return;
 }
-
 int main() {
-	ios_base::sync_with_stdio(0);
-	cin.tie(0); cout.tie(0);
-	cin >> n >> m;
-	for (int i = 0; i < n; i++)
-		parent[i] = i;
-	for (int i = 1; i < m + 1; i++) {
-		int v1, v2;
-		cin >> v1 >> v2;
-		if (get_parent(parent, v1) == v2 || get_parent(parent, v2) == v1 || get_parent(parent, v1) == get_parent(parent, v2)) {
-			cout << i;
-			exit(0);
-		}
-		union_parent(parent, v1, v2);
-	}
-	cout << 0;
-		
-	return 0;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int a, b;
+    cin >> v >> e;
+    for (int i = 0; i < v; i++){
+        parent[i] = i;
+    }
+    for (int i = 0; i < e; i++){
+        cin >> a >> b;
+        if (get_parent(parent, a) != get_parent(parent, b))
+            union_parent(parent, a, b);
+        else {
+            cout << i + 1 << endl;
+            return 0;
+        }
+    }
+    cout << "0" << endl;
+    return 0;
 }
