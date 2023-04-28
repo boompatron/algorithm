3f55549048151b0f9ffc0844fc42511cd90304bc
Bosub Kim <grapeflavoredapple@gmail.com>
2023-04-28 20:29:51 +0900

    [level 2] Title: 3월에 태어난 여성 회원 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
+++ 프로그래머스/lv2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
@@ -1,6 +1,6 @@
-select MEMBER_ID, MEMBER_NAME, GENDER, DATE_FORMAT(DATE_OF_BIRTH, '%Y-%m-%d') AS DATE_OF_BIRTH
-from MEMBER_PROFILE
-where MONTH(date_of_birth) = 3
-    and gender = 'W'
-    and tlno is not null
-order by member_id asc;
+SELECT MEMBER_ID, MEMBER_NAME, GENDER, DATE_FORMAT(DATE_OF_BIRTH, '%Y-%m-%d') as DATE_OF_BIRTH
+FROM MEMBER_PROFILE
+WHERE MONTH(DATE_OF_BIRTH) = 3
+    and GENDER = 'W'
+    and TLNO is not null
+ORDER BY MEMBER_ID
