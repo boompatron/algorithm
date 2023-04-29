7822a8b29921740382f5927da738a04ca2ef0896
Bosub Kim <grapeflavoredapple@gmail.com>
2023-04-29 19:06:49 +0900

    [level 1] Title: 폰켓몬, Time: 3.50 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,13 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+	public int solution(int[] nums) {
+		Set<Integer> s = new HashSet<>();
+		for (int num: nums){
+			s.add(num);
+		}
+
+		return Math.min(nums.length / 2, s.size());
+	}
+}
