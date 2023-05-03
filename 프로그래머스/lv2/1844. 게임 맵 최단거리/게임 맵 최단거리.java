7c15ea3ff97a4c615eda5f6d33d67b4c2201a13e
Bosub Kim <grapeflavoredapple@gmail.com>
2023-05-03 18:19:38 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.42 ms, Memory: 55.8 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,40 +1,53 @@
-import java.util.Deque;
-import java.util.LinkedList;
-class Solution {
-    private static int[] dxs = {1, -1, 0, 0}, dys = {0, 0, 1, -1};
-    private static int n, m;
-    private static int[][] distance;
+import java.util.*;
 
-    private static boolean inRange(int x, int y){
+class Solution {
+    int[] dxs = {1, -1, 0, 0}, dys = {0, 0, 1, -1};
+    int n, m;
+    
+    public boolean inRange(int x, int y){
         return 0 <= x && x < m && 0 <= y && y < n;
     }
+    
     public int solution(int[][] maps) {
-        n = maps.length; m = maps[0].length;
-        distance = new int[n][m];
-        for(int i = 0; i < n; i++) for(int j = 0; j < m; j++) distance[i][j] = Integer.MAX_VALUE;
-
+        n = maps.length;
+        m = maps[0].length;
+        
+        int[][] visited = new int[n][m];
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < m; j++)
+                visited[i][j] = n * m + 1;
+        }
+        
         Deque<Pos> dq = new LinkedList<>();
-        dq.add(new Pos(0, 0, 1));
-
-        while(!dq.isEmpty()){
-            Pos cur = dq.pop();
-            for(int i = 0; i < 4; i++){
-                int nx = cur.x + dxs[i], ny = cur.y + dys[i];
-                if(inRange(nx, ny) && maps[ny][nx] == 1 && distance[ny][nx] > cur.dis + 1){
-                    distance[ny][nx] = cur.dis + 1;
-                    dq.add(new Pos(nx, ny, cur.dis + 1));
+        dq.addFirst(new Pos(0, 0, 1));
+        
+        while (!dq.isEmpty()){
+            Pos cur = dq.poll();
+            for (int i = 0; i < 4; i++){
+                int nx = cur.x + dxs[i];
+                int ny = cur.y + dys[i];
+                int nz = cur.z + 1;
+                
+                if (inRange(nx, ny) && maps[ny][nx] == 1 && visited[ny][nx] > nz){
+                    dq.add(new Pos(nx, ny, nz));
+                    visited[ny][nx] = nz;
                 }
             }
         }
-
-        return distance[n - 1][m - 1] != Integer.MAX_VALUE ? distance[n - 1][m - 1] : -1;
+        
+        
+        
+        return visited[n - 1][m - 1] == n * m + 1 ? -1 : visited[n - 1][m - 1];
     }
 }
+
 class Pos{
-    public int x, y, dis;
-    public Pos(int x, int y, int dis){
+    public int x;
+    public int y;
+    public int z;
+    public Pos(int x, int y, int z){
         this.x = x;
         this.y = y;
-        this.dis = dis;
+        this.z = z;
     }
 }
