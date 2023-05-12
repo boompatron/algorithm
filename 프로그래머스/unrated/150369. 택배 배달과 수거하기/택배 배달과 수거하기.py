cc5b16493ba1c8ff4382ccaa93dc6d3a0d42869e
Bosub Kim <grapeflavoredapple@gmail.com>
2023-05-12 14:45:27 +0900

    [unrated] Title: 택배 배달과 수거하기, Time: 95.88 ms, Memory: 11.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
@@ -0,0 +1,33 @@
+def solution(cap, n, deliveries, pickups):
+    cnt = 0
+    del_pos, pick_pos = -1, -1
+    for i in range(n - 1, -1, -1):
+        if deliveries[i]:
+            del_pos = i
+            break
+    for i in range(n - 1, -1, -1):
+        print(pickups[i])
+        if pickups[i]:
+            pick_pos = i
+            break
+    while del_pos > -1 or pick_pos > -1:
+        cnt += max(del_pos, pick_pos) * 2 + 2
+        del_cap, pick_cap = cap, cap
+        while del_cap and del_pos > -1:
+            min_val = min(del_cap, deliveries[del_pos])
+            del_cap -= min_val
+            deliveries[del_pos] -= min_val
+            if not deliveries[del_pos]:
+                del_pos -= 1
+        while pick_cap and pick_pos > -1:
+            min_val = min(pick_cap, pickups[pick_pos])
+            pick_cap -= min_val
+            pickups[pick_pos] -= min_val
+            if not pickups[pick_pos]:
+                pick_pos -= 1
+
+        while not deliveries[del_pos] and del_pos > -1:
+            del_pos -= 1
+        while not pickups[pick_pos] and pick_pos > -1:
+            pick_pos -= 1
+    return cnt
