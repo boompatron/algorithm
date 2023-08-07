7a0d56855ca95d752aba0735ab89be26df36fefb
Bosub Kim <grapeflavoredapple@gmail.com>
2023-08-07 15:33:18 +0900

    [Gold III] Title: 파티, Time: 1228 ms, Memory: 72960 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -1,39 +1,32 @@
-import sys
-import heapq
+import sys, heapq
 INF = sys.maxsize
 
 
-def dijkstra(start, adj, ans):
-    ans[start] = 0
+n, m, x = map(int, sys.stdin.readline().rstrip().split())
+adj, ans = [[] for _ in range(n + 1)], [[INF for _ in range(n + 1)] for __ in range(n + 1)]
+for i in range(m):
+    a, b, c = map(int, sys.stdin.readline().rstrip().split())
+    adj[a].append([b, c])
+
+
+def dijkstra(num):
+    ans[num][num] = 0
     hq = []
-    heapq.heappush(hq, [0, start])
+    heapq.heappush(hq, [0, num])
     while hq:
         dis, idx = heapq.heappop(hq)
-        if ans[idx] < dis:
+        if ans[num][idx] < dis:
             continue
-        for a in adj[idx]:
-            next_idx, next_dis = a[0], a[1] + dis
-            if ans[next_idx] > next_dis:
-                ans[next_idx] = next_dis
-                heapq.heappush(hq, [next_dis, next_idx])
+        for aa in adj[idx]:
+            nidx, ndis = aa[0], aa[1] + dis
+            if ans[num][nidx] > ndis:
+                ans[num][nidx] = ndis
+                heapq.heappush(hq, [ndis, nidx])
 
 
-def solution():
-    v, e, x = map(int, sys.stdin.readline().rstrip().split())
-    adj, ans = [[] for _ in range(v + 1)], [[INF for _ in range(v + 1)] for __ in range(v + 1)]
-    while e:
-        a, b, c = map(int, sys.stdin.readline().rstrip().split())
-        adj[a].append([b, c])
-        e -= 1
-    for i in range(1, v + 1):
-        dijkstra(i, adj, ans[i])
-    answer = 0
-    for i in range(1, v + 1):
-        if i == x:
-            continue
-        answer = max(answer, ans[i][x] + ans[x][i])
-    print(answer)
-
-
-if __name__ == "__main__":
-    solution()
+for i in range(1, n + 1):
+    dijkstra(i)
+longest = -INF
+for i in range(1, n + 1):
+    longest = max(ans[i][x] + ans[x][i], longest)
+print(longest)
