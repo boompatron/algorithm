7b8b87e867c3adfb1e60970e574720ab01e18a60
Boseop Kim <grapeflavoredapple@gmail.com>
2024-04-06 12:31:39 +0900

    [Gold IV] Title: 연구소, Time: 4112 ms, Memory: 32512 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,33 +1,78 @@
 import sys
 from collections import deque
-from itertools import combinations
-dxs, dys = (1, -1, 0, 0), (0, 0, 1, -1)
-
+dxs, dys = [1, -1, 0, 0], [0, 0, 1, -1]
 n, m = map(int, sys.stdin.readline().rstrip().split())
 g = [list(map(int, sys.stdin.readline().rstrip().split())) for _ in range(n)]
-empty, empty_cnt, viruses, ans = set(), 0, set(), 0
-for i in range(n):
-    for j in range(m):
-        if g[i][j] == 0:
-            empty.add((j, i))
-            empty_cnt += 1
-        elif g[i][j] == 2:
-            viruses.add((j, i))
-for w1, w2, w3 in combinations(empty, 3):
-    visited = [[False for _ in range(m)] for __ in range(n)]
-    g[w1[1]][w1[0]] = g[w2[1]][w2[0]] = g[w3[1]][w3[0]] = 1
+d = [[123456789 for _ in range(m)] for __ in range(n)]
+visited = [[False for _ in range(m)] for __ in range(n)]
+
+
+def clear_visited():
+    for a in range(n):
+        for b in range(m):
+            visited[a][b] = False
+
+
+def in_range(a, b):
+    return 0 <= a < m and 0 <= b < n
+
+
+def virus_spread(a, b):
+    size = 1
     dq = deque()
-    for virus in viruses:
-        dq.appendleft(virus)
-    cnt = 0
+    dq.appendleft([a, b])
     while dq:
         x, y = dq.pop()
         for dx, dy in zip(dxs, dys):
             nx, ny = x + dx, y + dy
-            if 0 <= nx < m and 0 <= ny < n and g[ny][nx] == 0 and not visited[ny][nx]:
+            if in_range(nx, ny) and g[ny][nx] == 0 and not visited[ny][nx]:
+                dq.appendleft([nx, ny])
                 visited[ny][nx] = True
-                dq.appendleft((nx, ny))
-                cnt += 1
-    ans = max(ans, empty_cnt - cnt)
-    g[w1[1]][w1[0]] = g[w2[1]][w2[0]] = g[w3[1]][w3[0]] = 0
-print(ans - 3)
+                size += 1
+    #print(size)
+    return size
+
+
+wall, vacant, virus = 0, 0, 0
+for i in range(n):
+    for j in range(m):
+        if g[i][j] == 0:
+            vacant += 1
+        elif g[i][j] == 1:
+            wall += 1
+        else:
+            virus += 1
+ans = 123456789
+for first in range(n * m):
+    if g[first // m][first % m] == 0:
+        first_y, first_x = first // m, first % m
+    else:
+        continue
+    for second in range(first + 1, n * m):
+        if g[second // m][second % m] == 0:
+            second_y, second_x = second // m, second % m
+        else:
+            continue
+        for third in range(second + 1, n * m):
+            if g[third // m][third % m] == 0:
+                third_y, third_x = third // m, third % m
+            else:
+                continue
+            g[first_y][first_x] = 1
+            g[second_y][second_x] = 1
+            g[third_y][third_x] = 1
+            clear_visited()
+            tmp = 0
+            for i in range(n):
+                for j in range(m):
+                    if g[i][j] == 2 and not visited[i][j]:
+                        tmp += virus_spread(j, i)
+            ans = min(tmp, ans)
+            g[first_y][first_x] = 0
+            g[second_y][second_x] = 0
+            g[third_y][third_x] = 0
+            # print("first x, y : ", first_x, first_y, end="|")
+            # print("second x, y : ", second_x, second_y, end="|")
+            # print("third x, y : ", third_x, third_y, end="|")
+            # print("tmp : ", tmp)
+print(n * m - (wall + 3) - ans)
